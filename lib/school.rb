4d5765982030be9b6c4f17e4acb89ad831599ce1
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-06 17:41:49 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,48 @@
-# code here!
+require 'pry'
+
+class School 
+  attr_accessor :roster
+  roster = {}
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    if roster[grade]
+    else
+      roster[grade] = []
+    end # ends
+
+    if roster[grade].length == 0
+      roster[grade] << name
+    else
+      roster[grade].push(name)
+    end #Ends if statement
+  end #ends add_student method
+
+  def grade(grd)
+    roster[grd]
+  end #ends grade method
+
+   def sort
+    output = {}
+    roster.keys.sort.each do  | grade |
+      output[grade] = roster[grade].sort
+    end
+    output
+   end  # ends sort method
+
+end # ends School class
+
+ school = School.new("bhs")
+ school.add_student("Zach morris", 9)
+ school.add_student("AC Slater", 9)
+ school.add_student("Kelly Kapowski", 10)
+ school.add_student("Screech", 11)
+
+ binding.pry
